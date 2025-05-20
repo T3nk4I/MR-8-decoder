ac39a4c885f28b6f3d069f41a43f09bab783ff9e
T3nk4I <finnandjake1010@gmail.com>
2025-05-19 23:56:36 -0600

    adds motor control functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #define CH_A 6
 #define CH_B 7
 
+#define MA1 11
+#define MA2 6
+#define MB2 3
+#define MB1 5
+
 unsigned long pulse_duration_A; //min 1090 max 1950
 unsigned long pulse_duration_B;
 
@@ -24,5 +29,41 @@ void loop() {
   Serial.print(PWM_A);
   Serial.print("\t");
   Serial.println(PWM_B);
-  delay(100);
+  if (PWM_A = 128){
+    off();
+  }else if (PWM_A > 128){
+    //forward();
+  }else if (PWM_A < 128){
+    //backward();
+  }
+  
+  
+}
+
+void stop(){
+  analogWrite(MA1, 255);
+  analogWrite(MA2, 255);
+  analogWrite(MB1, 255);
+  analogWrite(MB2, 255);
+}
+
+void off(){
+  analogWrite(MA1, 0);
+  analogWrite(MA2, 0);
+  analogWrite(MB1, 0);
+  analogWrite(MB2, 0);
+}
+
+void forward(byte a, byte b){
+  analogWrite(MA1, a);
+  analogWrite(MA2, 0);
+  analogWrite(MB1, b);
+  analogWrite(MB2, 0);
+}
+
+void backward(byte a, byte b){
+  analogWrite(MA1, 0);
+  analogWrite(MA2, a);
+  analogWrite(MB1, 0);
+  analogWrite(MB2, b);
 }
